966c6674cc2e5897eee5e31d05d16283da821909
UchithmaSenevirathne <uchithma@gmail.com>
2023-10-29 22:20:22 +0530

    add customer form

--- src/main/java/lk/ijse/controller/CustomerManageFormController.java
+++ src/main/java/lk/ijse/controller/CustomerManageFormController.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TableColumn;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
@@ -16,6 +17,21 @@ public class CustomerManageFormController {
     private AnchorPane rootNode;
 
     @FXML
+    private TableColumn<?, ?> cusAddressCol;
+
+    @FXML
+    private TableColumn<?, ?> cusContactCol;
+
+    @FXML
+    private TableColumn<?, ?> cusEmailCol;
+
+    @FXML
+    private TableColumn<?, ?> cusIdCol;
+
+    @FXML
+    private TableColumn<?, ?> cusNameCol;
+
+    @FXML
     void btnDashboardOnAction(ActionEvent event) throws IOException {
         Parent rootNode = FXMLLoader.load(getClass().getResource("/view/DashboardForm.fxml"));
 
@@ -23,5 +39,17 @@ public class CustomerManageFormController {
         Stage stage = (Stage) this.rootNode.getScene().getWindow();
         stage.setTitle("Dashboard Form");
         stage.setScene(scene);
+    }
+
+    @FXML
+    void btnADDCusOnAction(ActionEvent event) throws IOException {
+        Parent rootNode = FXMLLoader.load(getClass().getResource("/view/AddCustomerForm.fxml"));
+
+        Scene scene = new Scene(rootNode);
+        Stage stage = new Stage();
+        stage.setScene(scene);
+        stage.setTitle("Customer Form");
+        stage.centerOnScreen();
+        stage.show();
     }
 }
