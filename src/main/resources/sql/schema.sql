f07a1c48b49ea0a056dd0add9ce436e0ecceabb7
UchithmaSenevirathne <uchithma@gmail.com>
2023-11-07 21:02:49 +0530

    content updated

--- src/main/resources/sql/schema.sql
+++ src/main/resources/sql/schema.sql
@@ -7,10 +7,8 @@ SHOW DATABASES;
 USE apexAutoRental;
 
 CREATE TABLE user(
-        uId VARCHAR(5) PRIMARY KEY,
-        name VARCHAR(20) NOT NULL,
-        userName VARCHAR(10) NOT NULL,
-        password VARCHAR(8) NOT NULL
+        userName VARCHAR(20) PRIMARY KEY,
+        password VARCHAR(20) NOT NULL
 );
 
 DESC user;
@@ -18,11 +16,13 @@ DESC user;
 SELECT * FROM user;
 
 CREATE TABLE customer(
-        id VARCHAR(5) PRIMARY KEY,
+        cusId VARCHAR(5) PRIMARY KEY,
         name VARCHAR(20) NOT NULL,
         address TEXT NOT NULL,
         email VARCHAR(20) NOT NULL,
-        contact VARCHAR(11) NOT NULL
+        contact VARCHAR(11) NOT NULL,
+        userName varchar(20),
+        constraint foreign key (userName) references user(userName)
 );
 
 DESC customer;
@@ -30,15 +30,14 @@ DESC customer;
 SELECT * FROM customer;
 
 CREATE TABLE driver(
-        id VARCHAR(5) PRIMARY KEY,
+        drId VARCHAR(5) PRIMARY KEY,
         name VARCHAR(20) NOT NULL,
         address TEXT NOT NULL,
         email VARCHAR(20) NOT NULL,
         contact VARCHAR(11) NOT NULL,
         licenseNo VARCHAR(20) NOT NULL,
-        userId varchar(5),
-        constraint foreign key (userId) references user(uId),
-        availability VARCHAR(10) NOT NULL
+        userName varchar(20),
+        constraint foreign key (userName) references user(userName)
 );
 
 DESC driver;
@@ -46,12 +45,12 @@ DESC driver;
 SELECT * FROM driver;
 
 CREATE TABLE worker(
-        id VARCHAR(5) PRIMARY KEY,
+        wId VARCHAR(5) PRIMARY KEY,
         name VARCHAR(20) NOT NULL,
         address TEXT NOT NULL,
         contact VARCHAR(11) NOT NULL,
-        userId varchar(5),
-        constraint foreign key (userId) references user(uId)
+        userName varchar(20),
+        constraint foreign key (userName) references user(userName)
 );
 
 DESC worker;
@@ -62,8 +61,8 @@ CREATE TABLE driverSalary(
         drSalId VARCHAR(5) PRIMARY KEY,
         amount DOUBLE (6,2) NOT NULL,
         month VARCHAR(20) NOT NULL,
-        driverId varchar(5),
-        constraint foreign key (driverId) references driver(id)
+        drId varchar(5),
+        constraint foreign key (drId) references driver(drId)
 );
 
 DESC driverSalary;
@@ -74,8 +73,8 @@ CREATE TABLE workerSalary(
         wSalId VARCHAR(5) PRIMARY KEY,
         amount DOUBLE (6,2) NOT NULL,
         month VARCHAR(20) NOT NULL,
-        driverId varchar(5),
-        constraint foreign key (driverId) references driver(id)
+        wId varchar(5),
+        constraint foreign key (wId) references worker(wId)
 );
 
 DESC workerSalary;
@@ -86,7 +85,7 @@ CREATE TABLE booking(
         bId VARCHAR(5) PRIMARY KEY,
         type VARCHAR(20) NOT NULL,
         cusId varchar(5),
-        constraint foreign key (cusId) references customer(id)
+        constraint foreign key (cusId) references customer(cusId)
 );
 
 DESC booking;
@@ -96,10 +95,10 @@ SELECT * FROM booking;
 CREATE TABLE driverSchedule(
         drSchedule VARCHAR(5) PRIMARY KEY,
         date DATE NOT NULL,
-        driverId varchar(5),
-        constraint foreign key (driverId) references driver(id),
-        bookingId varchar(5),
-        constraint foreign key (bookingId) references booking(bId),
+        drId varchar(5),
+        constraint foreign key (drId) references driver(drId),
+        bId varchar(5),
+        constraint foreign key (bId) references booking(bId),
         rideTo VARCHAR(20) NOT NULL,
         distance DOUBLE(5,2) NOT NULL
 );
@@ -111,8 +110,8 @@ SELECT * FROM driverSchedule;
 CREATE TABLE payment(
         payId VARCHAR(5) PRIMARY KEY,
         payAmount DOUBLE(10,2) NOT NULL,
-        bookingId varchar(5),
-        constraint foreign key (bookingId) references booking(bId)
+        bId varchar(5),
+        constraint foreign key (bId) references booking(bId)
 );
 
 DESC payment;
@@ -132,10 +131,10 @@ SELECT * FROM car;
 CREATE TABLE bookingDetail(
         bDetailId VARCHAR(5) PRIMARY KEY,
         date DATE NOT NULL,
-        carNumber varchar(5),
-        constraint foreign key (carNumber) references car(carNo),
-        bookingId varchar(5),
-        constraint foreign key (bookingId) references booking(bId),
+        carNo varchar(5),
+        constraint foreign key (carNo) references car(carNo),
+        bId varchar(5),
+        constraint foreign key (bId) references booking(bId),
         rideTo VARCHAR(20) NOT NULL,
         distance DOUBLE(5,2) NOT NULL,
         status VARCHAR(10)
@@ -157,10 +156,10 @@ SELECT * FROM carMaintain;
 CREATE TABLE maintainDetail(
         mainDetailId VARCHAR(5) PRIMARY KEY,
         date DATE NOT NULL,
-        carNumber varchar(5),
-        constraint foreign key (carNumber) references car(carNo),
-        maintainId varchar(5),
-        constraint foreign key (maintainId) references carMaintain(mainId),
+        carNo varchar(5),
+        constraint foreign key (carNo) references car(carNo),
+        mainId varchar(5),
+        constraint foreign key (mainId) references carMaintain(mainId),
         status VARCHAR(10)
 );
 
--- target/classes/sql/schema.sql
+++ target/classes/sql/schema.sql
@@ -6,21 +6,165 @@ SHOW DATABASES;
 
 USE apexAutoRental;
 
-CREATE TABLE customer(
-                         id VARCHAR(5) PRIMARY KEY,
-                         name VARCHAR(20) NOT NULL,
-                         address TEXT NOT NULL,
-                         email VARCHAR(20) NOT NULL,
-                         contact VARCHAR(11) NOT NULL
+CREATE TABLE user(
+        userName VARCHAR(20) PRIMARY KEY,
+        password VARCHAR(20) NOT NULL
 );
 
-CREATE TABLE driver(
-                         id VARCHAR(5) PRIMARY KEY,
-                         name VARCHAR(20) NOT NULL,
-                         address TEXT NOT NULL,
-                         email VARCHAR(20) NOT NULL,
-                         contact VARCHAR(11) NOT NULL,
-                         licenseNo VARCHAR(20) NOT NULL
+DESC user;
+
+SELECT * FROM user;
+
+CREATE TABLE customer(
+        cusId VARCHAR(5) PRIMARY KEY,
+        name VARCHAR(20) NOT NULL,
+        address TEXT NOT NULL,
+        email VARCHAR(20) NOT NULL,
+        contact VARCHAR(11) NOT NULL,
+        userName varchar(20),
+        constraint foreign key (userName) references user(userName)
 );
+
+DESC customer;
+
+SELECT * FROM customer;
+
+CREATE TABLE driver(
+        drId VARCHAR(5) PRIMARY KEY,
+        name VARCHAR(20) NOT NULL,
+        address TEXT NOT NULL,
+        email VARCHAR(20) NOT NULL,
+        contact VARCHAR(11) NOT NULL,
+        licenseNo VARCHAR(20) NOT NULL,
+        userName varchar(20),
+        constraint foreign key (userName) references user(userName)
+);
+
+DESC driver;
+
+SELECT * FROM driver;
+
+CREATE TABLE worker(
+        wId VARCHAR(5) PRIMARY KEY,
+        name VARCHAR(20) NOT NULL,
+        address TEXT NOT NULL,
+        contact VARCHAR(11) NOT NULL,
+        userName varchar(20),
+        constraint foreign key (userName) references user(userName)
+);
+
+DESC worker;
+
+SELECT * FROM worker;
+
+CREATE TABLE driverSalary(
+        drSalId VARCHAR(5) PRIMARY KEY,
+        amount DOUBLE (6,2) NOT NULL,
+        month VARCHAR(20) NOT NULL,
+        drId varchar(5),
+        constraint foreign key (drId) references driver(drId)
+);
+
+DESC driverSalary;
+
+SELECT * FROM driverSalary;
+
+CREATE TABLE workerSalary(
+        wSalId VARCHAR(5) PRIMARY KEY,
+        amount DOUBLE (6,2) NOT NULL,
+        month VARCHAR(20) NOT NULL,
+        wId varchar(5),
+        constraint foreign key (wId) references worker(wId)
+);
+
+DESC workerSalary;
+
+SELECT * FROM workerSalary;
+
+CREATE TABLE booking(
+        bId VARCHAR(5) PRIMARY KEY,
+        type VARCHAR(20) NOT NULL,
+        cusId varchar(5),
+        constraint foreign key (cusId) references customer(cusId)
+);
+
+DESC booking;
+
+SELECT * FROM booking;
+
+CREATE TABLE driverSchedule(
+        drSchedule VARCHAR(5) PRIMARY KEY,
+        date DATE NOT NULL,
+        drId varchar(5),
+        constraint foreign key (drId) references driver(drId),
+        bId varchar(5),
+        constraint foreign key (bId) references booking(bId),
+        rideTo VARCHAR(20) NOT NULL,
+        distance DOUBLE(5,2) NOT NULL
+);
+
+DESC driverSchedule;
+
+SELECT * FROM driverSchedule;
+
+CREATE TABLE payment(
+        payId VARCHAR(5) PRIMARY KEY,
+        payAmount DOUBLE(10,2) NOT NULL,
+        bId varchar(5),
+        constraint foreign key (bId) references booking(bId)
+);
+
+DESC payment;
+
+SELECT * FROM payment;
+
+CREATE TABLE car(
+        carNo VARCHAR(5) PRIMARY KEY,
+        brand VARCHAR(20) NOT NULL,
+        availability VARCHAR(10) NOT NULL
+);
+
+DESC car;
+
+SELECT * FROM car;
+
+CREATE TABLE bookingDetail(
+        bDetailId VARCHAR(5) PRIMARY KEY,
+        date DATE NOT NULL,
+        carNo varchar(5),
+        constraint foreign key (carNo) references car(carNo),
+        bId varchar(5),
+        constraint foreign key (bId) references booking(bId),
+        rideTo VARCHAR(20) NOT NULL,
+        distance DOUBLE(5,2) NOT NULL,
+        status VARCHAR(10)
+);
+
+DESC bookingDetail;
+
+SELECT * FROM bookingDetail;
+
+CREATE TABLE carMaintain(
+                    mainId VARCHAR(5) PRIMARY KEY,
+                    type VARCHAR(10) NOT NULL
+);
+
+DESC carMaintain;
+
+SELECT * FROM carMaintain;
+
+CREATE TABLE maintainDetail(
+        mainDetailId VARCHAR(5) PRIMARY KEY,
+        date DATE NOT NULL,
+        carNo varchar(5),
+        constraint foreign key (carNo) references car(carNo),
+        mainId varchar(5),
+        constraint foreign key (mainId) references carMaintain(mainId),
+        status VARCHAR(10)
+);
+
+DESC maintainDetail;
+
+SELECT * FROM maintainDetail;
 
 SHOW TABLES;
