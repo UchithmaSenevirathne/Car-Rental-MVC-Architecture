1bdfb8fc84c291ea7701dc5f0fd7c4e86856e4df
UchithmaSenevirathne <uchithma@gmail.com>
2023-11-07 21:22:06 +0530

    content updated

---
+++ src/main/java/lk/ijse/controller/CarManageFormController.java
@@ -0,0 +1,65 @@
+package lk.ijse.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.AnchorPane;
+
+public class CarManageFormController {
+
+    @FXML
+    private TableColumn<?, ?> colBrand;
+
+    @FXML
+    private TableColumn<?, ?> colCarNo;
+
+    @FXML
+    private TableColumn<?, ?> colColour;
+
+    @FXML
+    private AnchorPane rootNode;
+
+    @FXML
+    private TableView<?> tableView;
+
+    @FXML
+    void btnADDCarOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnBackOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnCustomerOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnDELETEOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnDashboardOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnDriverOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSEARCHOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnUPDATEOnAction(ActionEvent event) {
+
+    }
+}
